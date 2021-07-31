dee25ae9a8178c2e14a5af7054286241aeffa4ff
Dylan-Allison-Damitz <dylandamitz@gmail.com>
2021-07-31 16:35:53 -0700

    add README.md

---
+++ README.md
@@ -0,0 +1,41 @@
+# Super Galactic Age Calculator
+A space-age calculator that determines the user's age on various planets
+#### By _**Dylan Allison-Damitz**_
+
+## Technologies Used
+
+* Object-oriented JavaScript
+* Test-Driven Development
+* Jest
+
+## Description
+
+This website uses a single constructor and several prototype methods to return a user's age on Mercury, Venus, Mars, and Jupiter based upon the user's inputted age. This project was built using test-driven development methods, relying heavily on Jest to piece together each bit of functionality. 
+
+## Setup/Installation Requirements
+
+* Navigate to GitHub.com and search for profile "Dylan-Allison-Damitz"
+* Once you've navigated to "Dylan-Allison-Damitz" select repository named "super-galactic-age-calculator"
+* Inside of "super-galactic-age-calculator," click on the green "CODE" badge on the right side of the page and then copy the code by clicking on the clipboard icon
+* Once the code has been copied to your clipboard, navigate to your terminal and type command "cd ~" to return to your local drive. Next, navigate to your desktop by typing "cd desktop"
+* Once succesfully at your desktop in command terminal, you'll clone a copy of the project to your desktop by typing in terminal "git clone" and then copying https://github.com/Dylan-Allison-Damitz/super-galactic-age-calculator.git from your clipboard exactly one space after "git clone" in your command terminal
+* When your terminal command line says "git clone https://github.com/Dylan-Allison-Damitz/super-galactic-age-calculator.git" hit enter to copy the project to your desktop
+* In terminal, navigate to the newly created "super-galactic-age-calculator" directory by typing "cd super-galactic-age-calculator" 
+* Once inside "super-galactic-age-calculator", type "code ." into your terminal to launch the project in VSCode
+* After launching the the project, type "npm install" into your terminal while still inside of the "super-galactic-age-calculator" directory
+* Once npm is finished installing, you'll want to run the build by typing "npm run build" into the terminal
+* To test your work using Jest, type "npm run test" in terminal 
+
+## Known Bugs
+
+* No known bugs at this time
+
+## License
+
+MIT
+
+Copyright (c) 2021 Dylan Allison-Damitz
+
+## Contact Information
+
+dylandamitz@gmail.com
--- __tests__/SpaceAge.test.js
+++ __tests__/SpaceAge.test.js
@@ -12,39 +12,39 @@ describe("SpaceAge", () => {
         expect(earthYear.earthAge).toEqual(32);
     });
     test('should correctly return the users earth age in Mercury years', () => {
-      let mercuryYear = new SpaceAge(32, "Mercury");
-      expect(mercuryYear.planetCalculator()).toEqual(132.48);
+        let mercuryYear = new SpaceAge(32, "Mercury");
+        expect(mercuryYear.planetCalculator()).toEqual(132.48);
     });
     test('should correctly return the users earth age in Venus years', () => {
-      let venusYear = new SpaceAge(32, "Venus");
-      expect(venusYear.planetCalculator()).toEqual(51.84);
+        let venusYear = new SpaceAge(32, "Venus");
+        expect(venusYear.planetCalculator()).toEqual(51.84);
     });
     test('should correctly return the users earth age in Mars years', () => {
-      let marsYear = new SpaceAge(32, "Mars");
-      expect(marsYear.planetCalculator()).toEqual(16.96);
+        let marsYear = new SpaceAge(32, "Mars");
+        expect(marsYear.planetCalculator()).toEqual(16.96);
     });
     test('should correctly return the users earth age in Jupiter years', () => {
-      let jupiterYear = new SpaceAge(32, "Jupiter");
-      expect(jupiterYear.planetCalculator()).toEqual(2.56);
+        let jupiterYear = new SpaceAge(32, "Jupiter");
+        expect(jupiterYear.planetCalculator()).toEqual(2.56);
     });
     test('should correctly return the users life expectancy on Mercury', () => {
-      let spanMercury = new SpaceAge(32, "Mercury");
-      expect(spanMercury.lifeSpan()).toEqual(-47.36);
+        let spanMercury = new SpaceAge(32, "Mercury");
+        expect(spanMercury.lifeSpan()).toEqual(-47.36);
     });
     test('should correctly return the users life expectancy on Venus', () => {
-      let spanVenus = new SpaceAge(32, "Venus");
-      expect(spanVenus.lifeSpan()).toEqual(33.16);
+        let spanVenus = new SpaceAge(32, "Venus");
+        expect(spanVenus.lifeSpan()).toEqual(33.16);
     });
     test('should correctly return the users life expectancy on Mars', () => {
-      let spanMars = new SpaceAge(32, "Mars");
-      expect(spanMars.lifeSpan()).toEqual(68.36);
+        let spanMars = new SpaceAge(32, "Mars");
+        expect(spanMars.lifeSpan()).toEqual(68.36);
     });
     test('should correctly return the users life expectancy on Jupiter', () => {
-      let spanJupiter = new SpaceAge(32, "Jupiter");
-      expect(spanJupiter.lifeSpan()).toEqual(82.44);
+        let spanJupiter = new SpaceAge(32, "Jupiter");
+        expect(spanJupiter.lifeSpan()).toEqual(82.44);
     });
     test('should correctly return years lived past lifespan on Mercury', () => {
-      let gonePastMercury = new SpaceAge(32, "Mercury");
-      expect(gonePastMercury.yearsPast()).toEqual(47.16);
+        let gonePastMercury = new SpaceAge(32, "Mercury");
+        expect(gonePastMercury.yearsPast()).toEqual(47.16);
     });
 });
